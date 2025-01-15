1fe33d75ec5a830944c69fe1c8ec929e250910f2
MD Mahabub Rahman <100031330+mahabubDevs@users.noreply.github.com>
2025-01-15 18:11:34 +0600

    Delete frontend directory

--- frontend/src/component/Home/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface LayoutPropos{
-    children: ReactNode;
-}
-
-export default function Layout({ children }: LayoutPropos) {
-    return <>{children }</>
-}
